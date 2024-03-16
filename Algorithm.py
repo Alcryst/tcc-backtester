8311f65896deb7ecd870a629f9c49c275d934544
XuJeff04 <jeffery.xu.2023@gmail.com>
2024-03-15 23:20:50 -0500

    algorithm class of backtester

---
+++ Algorithm.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class Algorithm:
+    def __init__(self):
+        return
+
+    def run(histData: np.ndarray, **kwargs):
+        raise NotImplementedError("implement the run method")
+
+    def cache(self, args):
+        pass
