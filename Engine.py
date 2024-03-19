f5c52cd9fc9159630312a83fa64e2e13c3aa1963
Saahir Narang <saahir.narang@gmail.com>
2024-03-18 23:54:02 -0500

    added engine class file

---
+++ Engine.py
@@ -0,0 +1,9 @@
+
+class Engine:
+    def __init__(self, startingValue, histDataLen):
+        # to do 
+        pass
+        
+    def run(ticker, start, end, step, algo):
+        # to do
+        pass 
